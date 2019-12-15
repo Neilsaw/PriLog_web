159517b449fe30243d560ecb58ed557bc41bd4a4
Neilsaw <kondouotu@gmail.com>
2019-12-15 23:50:55 +0900

    add make source code

---
+++ picture_to_mask.py
@@ -0,0 +1,158 @@
+import cv2
+import numpy as np
+from PIL import Image
+import os, glob
+
+# 画像が保存されているルートディレクトリのパス
+root_dir = "../../LearningData/UB_name_master"
+# キャラクター名
+characters = [
+    "アオイ",
+    "アオイ(編入生)",
+    "アカリ",
+    "アキノ",
+    "アヤネ",
+    "アヤネ(クリスマス)",
+    "アユミ",
+    "アリサ",
+    "アン",
+    "アンナ",
+    "イオ",
+    "イオ(☆6以降)",
+    "イオ(サマー)",
+    "イリヤ",
+    "エミリア",
+    "エリコ",
+    "エリコ(バレンタイン)",
+    "カオリ",
+    "カオリ(サマー)",
+    "カスミ",
+    "カヤ",
+    "キャル",
+    "キャル(☆6以降)",
+    "キャル(サマー)",
+    "キョウカ",
+    "キョウカ(ハロウィン)",
+    "クウカ",
+    "クウカ(オーエド)",
+    "クリスティーナ",
+    "クリスティーナ(クリスマス)",
+    "クルミ",
+    "クルミ(クリスマス)",
+    "グレア",
+    "クロエ",
+    "コッコロ",
+    "コッコロ(☆6以降)",
+    "コッコロ(サマー)",
+    "サレン",
+    "サレン(サマー)",
+    "ジータ",
+    "シオリ",
+    "シズル",
+    "シズル(バレンタイン)",
+    "シノブ",
+    "シノブ(ハロウィン)",
+    "ジュン",
+    "スズナ",
+    "スズナ(サマー)",
+    "スズメ",
+    "スズメ(サマー)",
+    "タマキ",
+    "タマキ(サマー)",
+    "チカ",
+    "チカ(クリスマス)",
+    "ツムギ",
+    "トモ",
+    "ナナカ",
+    "ニノン",
+    "ニノン(オーエド)",
+    "ネネカ",
+    "ノゾミ",
+    "ノゾミ(クリスマス)",
+    "ハツネ",
+    "ヒヨリ",
+    "ヒヨリ(ニューイヤー)",
+    "ペコリーヌ",
+    "ペコリーヌ(☆6以降)",
+    "ペコリーヌ(サマー)",
+    "マコト",
+    "マコト(サマー)",
+    "マツリ",
+    "マヒル",
+    "マホ",
+    "マホ(サマー)",
+    "ミサキ",
+    "ミサキ(ハロウィン)",
+    "ミサト",
+    "ミソギ",
+    "ミソギ(ハロウィン)",
+    "ミツキ",
+    "ミフユ",
+    "ミフユ(サマー)",
+    "ミミ",
+    "ミミ(ハロウィン)",
+    "ミヤコ",
+    "ミヤコ(ハロウィン)",
+    "ムイミ",
+    "モニカ",
+    "ユイ",
+    "ユイ(ニューイヤー)",
+    "ユカリ",
+    "ユキ",
+    "ヨリ",
+    "ラム",
+    "リノ",
+    "リノ(☆6以降)",
+    "リマ",
+    "リマ(☆6以降)",
+    "リン",
+    "ルゥ",
+    "ルカ",
+    "ルナ",
+    "レイ",
+    "レイ(ニューイヤー)",
+    "レム",
+]
+
+
+# 画像データ用配列
+X = []
+# ラベルデータ用配列
+Y = []
+
+
+# 画像データごとにadd_sample()を呼び出し、X,Yの配列を返す関数
+def make_sample(files):
+    global X, Y
+    X = []
+    Y = []
+    for cat, fname in files:
+        add_sample(cat, fname)
+    return np.array(X), np.array(Y)
+
+
+# 渡された画像データを読み込んでXに格納し、また、
+# 画像データに対応するcategoriesのidxをY格納する関数
+def add_sample(cat, fname):
+    img = Image.open(fname)
+    data = np.asarray(img)
+    data_gray = cv2.cvtColor(data, cv2.COLOR_RGB2GRAY)
+    ret, result = cv2.threshold(data_gray, 200, 255, cv2.THRESH_BINARY)
+    invResult = cv2.bitwise_not(result)
+    cv2.imwrite('save_data/ ' + str(cat) + '.png', invResult)
+    X.append(invResult)
+    Y.append(cat)
+
+# 全データ格納用配列
+allfiles = []
+
+# カテゴリ配列の各値と、それに対応するidxを認識し、全データをallfilesにまとめる
+for idx, cat in enumerate(characters):
+    image_dir = root_dir + "/" + cat
+    files = glob.glob(image_dir + "/*.png")
+    for f in files:
+        allfiles.append((idx, f))
+
+X_train, y_train = make_sample(allfiles)
+# データを保存する（データの名前を「UB_ALL.npy」としている）
+np.save("model/UB_name.npy", X_train)
