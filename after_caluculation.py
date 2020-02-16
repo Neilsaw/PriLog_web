00133ab69f2fbd6f23b70231e8fd3ff98a4ce20e
Neilsaw <kondouotu@gmail.com>
2020-02-16 21:11:55 +0900

    ・不具合修正 クロエが居る場合、算出エラーが発生する不具合修正
    float型をint型に変換する処理追加

--- after_caluculation.py
+++ after_caluculation.py
@@ -22,27 +22,30 @@ def make_ub_value_list(ub_data, characters):
     debuff_value = []
 
     for i in characters:
-        s1_value = (np.array(db.s1_table[i]) * db.value_table[i][cd.S1][VALUE])
-        s2_value = (np.array(db.s2_table[i]) * db.value_table[i][cd.S2][VALUE])
+        s1_value = np.array(db.s1_table[i]) * db.value_table[i][cd.S1][VALUE]
+        s2_value = np.array(db.s2_table[i]) * db.value_table[i][cd.S2][VALUE]
+
+        s1_int_value = s1_value.astype(int)
+        s2_int_value = s2_value.astype(int)
 
         s1_type = db.value_table[i][cd.S1][VALUE_TYPE]
         s2_type = db.value_table[i][cd.S2][VALUE_TYPE]
 
         if s1_type == cd.PHYSICAL:
-            physical_debuff_list += s1_value
+            physical_debuff_list += s1_int_value
         elif s1_type == cd.MAGICAL:
-            magical_debuff_list += s1_value
+            magical_debuff_list += s1_int_value
         elif s1_type == cd.PHYSICAL_AND_MAGICAL:
-            physical_debuff_list += s1_value
-            magical_debuff_list += s1_value
+            physical_debuff_list += s1_int_value
+            magical_debuff_list += s1_int_value
 
         if s2_type == cd.PHYSICAL:
-            physical_debuff_list += s2_value
+            physical_debuff_list += s2_int_value
         elif s2_type == cd.MAGICAL:
-            magical_debuff_list += s2_value
+            magical_debuff_list += s2_int_value
         elif s2_type == cd.PHYSICAL_AND_MAGICAL:
-            physical_debuff_list += s2_value
-            magical_debuff_list += s2_value
+            physical_debuff_list += s2_int_value
+            magical_debuff_list += s2_int_value
 
     ub_count = range(len(ub_data))
 
@@ -53,14 +56,15 @@ def make_ub_value_list(ub_data, characters):
         ub_base_value = db.value_table[characters_num][cd.UB][VALUE]
 
         if ub_start_time < (BATTLE_LENGTH - 18):
-            ub_value = (np.array(db.ub_table[characters_num]) * ub_base_value)
+            ub_value = np.array(db.ub_table[characters_num]) * ub_base_value
             ub_value = np.append([0] * ub_start_time, ub_value)
             ub_value = np.append(ub_value, [0] * (BATTLE_LENGTH - (ub_start_time + 18)))
         else:
-            ub_value = (np.array(db.ub_table[characters_num]) * ub_base_value)
+            ub_value = np.array(db.ub_table[characters_num]) * ub_base_value
             ub_value = ub_value[:ub_time]
             ub_value = np.append([0] * ub_start_time, ub_value)
 
+        ub_int_value = ub_value.astype(int)
         ub_value_type = cd.ub_type_table[characters_num]
 
         if 0 < ub_time <= 90:
@@ -72,12 +76,12 @@ def make_ub_value_list(ub_data, characters):
             ub_type = db.value_table[characters_num][cd.UB][VALUE_TYPE]
 
             if ub_type == cd.PHYSICAL:
-                physical_debuff_list = physical_debuff_list + ub_value
+                physical_debuff_list = physical_debuff_list + ub_int_value
             elif ub_type == cd.MAGICAL:
-                magical_debuff_list = magical_debuff_list + ub_value
+                magical_debuff_list = magical_debuff_list + ub_int_value
             elif ub_type == cd.PHYSICAL_AND_MAGICAL:
-                physical_debuff_list = physical_debuff_list + ub_value
-                magical_debuff_list = magical_debuff_list + ub_value
+                physical_debuff_list = physical_debuff_list + ub_int_value
+                magical_debuff_list = magical_debuff_list + ub_int_value
         else:
             debuff_value.append("???")
 
