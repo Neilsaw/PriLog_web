1adb91843a9b437eebf7c1209eb18f82464cca9a
tokitzkaze <60331594+tokitzkaze@users.noreply.github.com>
2020-05-06 22:20:56 -0700

    311/410/x99エラー実装
    311/410/x99エラー追加
    3xxの定義をTemporary Unableに修正
    399一時的な例外
    499恒久的例外
    599認証・呼出に伴う例外

--- error_list.py
+++ error_list.py
@@ -8,39 +8,45 @@
 import numpy as np
 
 
-# status code
-# 2xx success
+# Status Code
+# 2xx Success
 DONE = 200
 DONE_IN_SD = 201
 
-# 3xx temporarily failure
+# 3xx Temporary Unable
 TMP_DONE_IN_SD = 301
+ERR_INCOMPLETE_IN_SD = 311
 ERR_CANT_GET_HD = 312
 ERR_ANALYZE_TIMEOUT = 313
+ERR_TMP_UNEXPECTED = 399
 
-# 4xx Definite failure
+# 4xx Definite Failure
 ERR_BAD_URL = 412
 ERR_BAD_LENGTH = 413
 ERR_BAD_RESOLUTION = 414
-ERR_UNEXPECTED = 499
+ERR_PERM_UNEXPECTED = 499
 
-# 5xx api request failure
+# 5xx API Request Failure
 ERR_BAD_REQ = 522
 ERR_BAD_TOKEN = 523
+ERR_REQ_UNEXPECTED = 599
 
 
 error_list = [
     [DONE, "OK"],
-    [DONE_IN_SD, "SD画質での解析です"],
-    [TMP_DONE_IN_SD, "SD画質での解析です。5分経過後に再度解析をお願いします"],
-    [ERR_CANT_GET_HD, "動画の取得に失敗しました。もう一度入力をお願いしま"],
-    [ERR_ANALYZE_TIMEOUT, "解析がタイムアウトしました。もう一度入力をお願いします"],
-    [ERR_BAD_URL, "URLはhttps://www.youtube.com/watch?v=...の形式でお願いします"],
-    [ERR_BAD_LENGTH, "動画時間が長すぎるため、解析に対応しておりません"],
-    [ERR_BAD_RESOLUTION, "非対応の動画です。720pの一部の動画に対応しております"],
-    [ERR_UNEXPECTED, "解析結果の取得に失敗しました"],
-    [ERR_BAD_REQ, "必須パラメータがありません"],
-    [ERR_BAD_TOKEN, "不正なトークンです　twitter @PriLog_R までご連絡下さい"]
+    [DONE_IN_SD, "SD画質での解析です。"],
+    [TMP_DONE_IN_SD, "SD画質での解析です。5分以上経過後に再度解析を試みられます。"],
+    [ERR_INCOMPLETE_IN_SD, "SD画質での解析に失敗しました。5分以上経過後に再度解析を試みられます。"],
+    [ERR_CANT_GET_HD, "動画の取得に失敗しました。5分以上経過後に再度解析を試みられます。"],
+    [ERR_ANALYZE_TIMEOUT, "解析がタイムアウトしました。5分以上経過後に再度解析を試みられます。"],
+    [ERR_TMP_UNEXPECTED, "一時的に解析出来ません。5分以上経過後に再度解析を試みられます。"],
+    [ERR_BAD_URL, "URLはhttps://www.youtube.com/watch?v=...の形式でお願いします。"],
+    [ERR_BAD_LENGTH, "動画時間が長すぎるため、解析に対応しておりません。"],
+    [ERR_BAD_RESOLUTION, "非対応の解像度です。720pの一部の動画に対応しております。"],
+    [ERR_PERM_UNEXPECTED, "解析出来ない動画です。"],
+    [ERR_BAD_REQ, "必須パラメータがありません。"],
+    [ERR_BAD_TOKEN, "不正なトークンです。　twitter @PriLog_R までご連絡下さい。"],
+    [ERR_REQ_UNEXPECTED, "API処理中に予期しない問題が起きました。　twitter @PriLog_R までご連絡下さい。"]
 ]
 
 
